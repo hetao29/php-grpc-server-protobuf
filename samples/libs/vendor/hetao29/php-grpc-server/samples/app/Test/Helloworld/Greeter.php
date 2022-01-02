da7779171260df561d3213bb0e0678e8a85b77fb
hetao <hetao@hetao.name>
2022-01-02 22:51:59 +0800

    update samples

--- samples/libs/vendor/hetao29/php-grpc-server/samples/app/Test/Helloworld/Greeter.php
+++ samples/libs/vendor/hetao29/php-grpc-server/samples/app/Test/Helloworld/Greeter.php
@@ -5,7 +5,7 @@ class Greeter{
 	 * @return Test\Helloworld\HelloReply
 	 */
 	public function SayHello($data){
-		$request = \GRpcServer::deserializeMessage([HelloRequest::class, null], $data);
+		$request = \GRpcServer::decode(HelloRequest::class, $data);
 		$reply = new HelloReply();
 		$reply->setMessage("Hello, ".$request->getName()."!");
 		return $reply;
