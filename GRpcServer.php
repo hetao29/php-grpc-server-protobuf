80785e130168da52ee7fd1c9a7ebb465fea5c93e
hetao <hetao@hetao.name>
2022-01-06 08:56:47 +0800

    支持swoole

--- GRpcServer.php
+++ GRpcServer.php
@@ -16,87 +16,86 @@
 /**
  * @package GRpcServer
  */
-use Google\Protobuf\Internal\Message;
-if(!class_exists("GRpcServer",false)):
-	final class GRpcServer{
+final class GRpcServer{
 
-		/**
-		 * main method!
-		 *
-		 * @return false | binary str
-		 */
-		public static function run(){
-			$data = self::getRawData();
-			$uri = $_SERVER['REQUEST_URI'] ?? "";
-			$class_name = str_replace(["/","."],["","\\"],dirname($uri));
-			$func_name = basename($uri);
-			header('content-type: application/grpc+proto');
-			try{
-				$ref = new ReflectionClass($class_name);
-				$params = $ref->getMethod($func_name)->getParameters();
-				if($params){
-					$param_type = $params[0]->getType();
-					if($param_type){
-						$param_name= $param_type->getName();;
-						$ref_param = new ReflectionClass($param_name);
-						if($ref_param->hasMethod("mergeFromString")){
-							$class = new $class_name();
-							$request = self::decode($param_name,$data);
-							$response = $class->$func_name($request);
-							if(method_exists($response,"serializeToString")){
-								header("grpc-status: 0");
-								return self::encode($response);
-							}else{
-								$type = gettype($response);
-								header("grpc-message: The Return value type $type of $class_name::$func_name() is wrong");
-							}
+	/**
+	 * main method!
+	 *
+	 * @return false | binary str
+	 */
+	public static function run($uri = null, $data = null){
+		$data = $data ?? self::getRawData();
+		$uri = $uri ?? $_SERVER['REQUEST_URI'] ?? "";
+		$class_name = str_replace(["/","."],["","\\"],dirname($uri));
+		$func_name = basename($uri);
+		$code=0;$msg="";
+		try{
+			$ref = new ReflectionClass($class_name);
+			$params = $ref->getMethod($func_name)->getParameters();
+			if($params){
+				$param_type = $params[0]->getType();
+				if($param_type){
+					$param_name= $param_type->getName();;
+					$ref_param = new ReflectionClass($param_name);
+					if($ref_param->hasMethod("mergeFromString")){
+						$class = new $class_name();
+						$request = self::decode($param_name,$data);
+						$response = $class->$func_name($request);
+						if(method_exists($response,"serializeToString")){
+							return self::encode($response);
 						}else{
-							header("grpc-message: The {$params[0]} of $class_name::$func_name() type is wrong");
+							$type = gettype($response);
+							$code = -1;
+							$msg = "grpc-message: The Return value type $type of $class_name::$func_name() is wrong";
 						}
 					}else{
-						header("grpc-message: The {$params[0]} of $class_name::$func_name() type have not defined");
+						$code = -2;
+						$msg = "grpc-message: The {$params[0]} of $class_name::$func_name() type is wrong";
 					}
 				}else{
-					header("grpc-message: The Parameter of $class_name::$func_name() is empty");
+					$code = -3;
+					$msg = "grpc-message: The {$params[0]} of $class_name::$func_name() type have not defined";
 				}
-			}catch(ReflectionException $e){
-				header("grpc-message: ".$e->getMessage());
+			}else{
+				$code = -4;
+				$msg = "grpc-message: The Parameter of $class_name::$func_name() is empty";
 			}
-			header("grpc-status: 2");
-			header('HTTP/1.1 502 Internal Server Error');
+		}catch(Exception $e){
+			$code = -5;
+			$msg = "grpc-message: ".$e->getMessage();
+		}
+		throw new Exception($msg,$code);
+	}
+
+	public static function getRawData(){
+		$data=null;
+		if(isset($GLOBALS['HTTP_RAW_POST_DATA'])){
+			$data = $GLOBALS['HTTP_RAW_POST_DATA'];
+		}else{
+			$data = file_get_contents("php://input");
+		}
+		return $data;
+	}
+
+	//https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md
+	public static function encode($obj){
+		$out= $obj->serializeToString();
+		return pack("CN", 0, strlen($out)) . $out;
+	}
+
+	public static function decode($className, string $body){
+		if(empty($body)){
 			return false;
 		}
+		$array = unpack("Cflag/Nlength", $body);
 
-		public static function getRawData(){
-			$data=null;
-			if(isset($GLOBALS['HTTP_RAW_POST_DATA'])){
-				$data = $GLOBALS['HTTP_RAW_POST_DATA'];
-			}else{
-				$data = file_get_contents("php://input");
-			}
-			return $data;
+		if($array==false){
+			return false;
 		}
+		$message = substr($body, 5, $array['length']);
 
-		//https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md
-		public static function encode($obj){
-			$out= $obj->serializeToString();
-			return pack("CN", 0, strlen($out)) . $out;
-		}
-
-		public static function decode($className, string $body){
-			if(empty($body)){
-				return false;
-			}
-			$array = unpack("Cflag/Nlength", $body);
-
-			if($array==false){
-				return false;
-			}
-			$message = substr($body, 5, $array['length']);
-
-			$obj = new $className();
-			$obj->mergeFromString($message);
-			return $obj;
-		}
+		$obj = new $className();
+		$obj->mergeFromString($message);
+		return $obj;
 	}
-endif;
+}
